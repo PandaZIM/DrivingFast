419619272aa61498b262b51e6e12d21b41fca096
pandazim <jaotoledo74@gmail.com>
2020-05-27 04:02:06 -0400

    Server done

---
+++ src/app/models/Users.js
@@ -0,0 +1,13 @@
+import Sequelize, { Model, Sequelize } from "sequelize"
+
+class User extends Model{
+    static init(sequelize){
+        super.init(
+            {
+
+            }
+        )
+    }
+}
+
+export default User
